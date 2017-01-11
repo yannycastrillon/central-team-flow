afcb5a9aeca7fe681e98df10d242f44def7c2adc
Yanny Castrillon <yannyandres117@gmail.com>
2017-01-11 12:56:54 -0800

    add middleware code. add npm bodyparser and morgan

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 var
   express = require("express"),
   app = express(),
-  mongoose = require("mongoose")
+  mongoose = require("mongoose"),
+  bodyParser = require('body-parser'),
+  logger = require("morgan")
+
+ // Middleware
+ app.use(logger('dev'));
+ app.use(bodyParser.json());
