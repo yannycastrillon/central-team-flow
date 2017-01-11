bde7443f299e997af542fa22a767086a1e84a24d
Michael <Michael.Bardwell.Scott@Gmail.com>
2017-01-11 12:54:24 -0800

    adding mongoose connection

--- server.js
+++ server.js
@@ -2,3 +2,7 @@ var
   express = require("express"),
   app = express(),
   mongoose = require("mongoose")
+
+mongoose.connect('mongodb://localhost/#', (err) => {
+	console.log(err || 'Connected')
+})
