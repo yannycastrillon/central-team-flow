13e526d95cb690de2ca78e7f71b261fdf4f4a470
justinhessdev <justinhessdev@gmail.com>
2017-01-11 12:55:59 -0800

    added listen to port 3000

--- server.js
+++ server.js
@@ -2,3 +2,20 @@ var
   express = require("express"),
   app = express(),
   mongoose = require("mongoose")
+
+
+// because i felt like it
+function reverse(myString) {
+  var reverseString=""
+
+   for(var i=0; i<myString.length; i++){
+     reverseString = myString[i] + reverseString
+   }
+
+   return reverseString
+}
+
+  console.log(reverse("Better Hello World"));
+
+
+app.listen(3000, ( () => "Server running on port 3000"))
